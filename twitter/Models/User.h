564a3b7bc99a8208b812a9b9f078793b47994b10
Daniela Guajardo <danyguajiba@fb.com>
2019-07-01 16:12:06 -0700

    user can log in and begining of user classes

---
+++ twitter/Models/User.h
@@ -0,0 +1,27 @@
+//
+//  User.h
+//  twitter
+//
+//  Created by danyguajiba on 7/1/19.
+//  Copyright © 2019 Emerson Malca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface User : NSObject
+
+
+// TODO: Add properties, all public properties for the User go here
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *screenName;
+
+// TODO: Create initializer
+//we set up as a dictionary because that is how we import the information
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
