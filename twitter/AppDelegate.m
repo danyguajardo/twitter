75c2d4500cf879a044b69f9d73a66dc585e9900b
Daniela Guajardo <danyguajiba@fb.com>
2019-07-05 12:25:40 -0700

    the tab bar already works!

--- twitter/AppDelegate.m
+++ twitter/AppDelegate.m
@@ -20,7 +20,7 @@
     
     if ([[APIManager shared] isAuthorized]) {
         UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-        UINavigationController *navigationController = [storyboard instantiateViewControllerWithIdentifier:@"TweetsNavigationController"];
+        UINavigationController *navigationController = [storyboard instantiateViewControllerWithIdentifier:@"tabBarController"];
         self.window.rootViewController = navigationController;
     }
     
